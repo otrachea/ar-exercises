ed5940b91a8aacb859e406339630566952d78cbe
otrachea <francesca.ho459@gmail.com>
2022-05-02 20:53:35 +0000

    Finish exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+def print_loop (arr)
+  arr.each do |item|
+    puts "#{item.name}, #{item.annual_revenue}"
+  end
+end
+
+mens_stores = Store.where(mens_apparel: true)
+print_loop(mens_stores)
+
+womens_stores = Store.where(womens_apparel: true, annual_revenue: ..1000000 )
+print_loop(womens_stores)
